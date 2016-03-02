52b5ec67b6ee543f9ee4aecab1461fc1f6276f26
root <root@smith-X4.(none)>
2016-03-02 17:05:04 +0800

    no change, just sync with pc

--- cv/serial_read.py
+++ cv/serial_read.py
@@ -1,10 +1,12 @@
 import serial
 import sys
+import time
 
-with serial.Serial('/dev/ttyUSB0',115200)  as ser:
+with serial.Serial('/dev/ttyUSB0',9600)  as ser:
     while(True):
-        data = ser.readall()
-        print data
+        data = ser.read(7)
+        print (str)(data)
+	time.sleep(0.1)
         if len(data) >= 5:
             if (data[0] == '#'):
                 data = data[1:]
