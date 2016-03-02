0ffe52d5b3ff889b294308ea942e8d00e2d01746
root <root@smith-X4.(none)>
2016-03-02 12:28:12 +0800

    some small changes on serial

--- cv/serial_read.py
+++ cv/serial_read.py
@@ -1,10 +1,10 @@
 import serial
 import sys
 
-with serial.Serial('/dev/tty.usbserial-A4439N8',115200)  as ser:
+with serial.Serial('/dev/ttyUSB0',115200)  as ser:
     while(True):
-        data = ser.readline()
-        #print data
+        data = ser.readall()
+        print data
         if len(data) >= 5:
             if (data[0] == '#'):
                 data = data[1:]
