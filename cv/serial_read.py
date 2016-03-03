bd9e1b6b5db91750e2c3b23fc5e2c71ea703596c
root <root@smith-X4.(none)>
2016-03-03 14:28:10 +0800

    small changes

--- cv/serial_read.py
+++ cv/serial_read.py
@@ -7,7 +7,7 @@ with serial.Serial('/dev/ttyUSB0',115200)  as ser:
         data = ser.readline()
         print data
 	#print len(data)
-	time.sleep(0.1)
+	time.sleep(0.001)
         if len(data) >= 5:
             if (data[0] == '#'):
                 data = data[1:]
@@ -17,11 +17,11 @@ with serial.Serial('/dev/ttyUSB0',115200)  as ser:
                 ser.flush
 	    else:
 		ser.close()
-		time.sleep(0.1)
+		time.sleep(0.001)
 		ser.open()
 	else:
 	    ser.close()
-	    time.sleep(0.1)
+	    time.sleep(0.001)
 	    ser.open()
 		
 	
