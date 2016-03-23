4022c55795919943adb0f19a16f5aae7a79ebca8
root <root@smith-X4.(none)>
2016-03-23 21:06:23 +0800

    [ADD]add test script

--- cv/catchtest/cv_chase.py
+++ cv/catchtest/cv_chase.py
@@ -8,38 +8,43 @@ import time
 from cv_interface import videoHandle as myv
 from move_interface import carHandle as myc
 
-max_speed = 30
+max_speed = 70
 result = {'angle':0}
 cam = myv(0)
 car = myc(0)
+re_size = (120 ,90)
 
 cam.select_image_color()
 cv2.destroyAllWindows()
-time.sleep(5)
-print "start now"
+#time.sleep(5)
+#print "start now"
 while(True):
     cam.get_image()
-    cam.prehandle_image(size = (80, 60))
+    cam.prehandle_image(size = re_size)
 	
     #cam.prehandle_image()
     cam.findcenter_image()
     #detect the glob
     if cam.moments['m00'] != 0:
         #cv2.line(cam.frame, (cam.centerx,cam.centery), (x_pre, y_pre), (255,0,0),3) 
-        result = cam.generate_output(point2 = {'x':cam.centerx,'y':cam.centery}, point1 = {'x':40,'y':30}) 
+        result = cam.generate_output(point2 = {'x':cam.centerx,'y':cam.centery}, point1 = {'x':re_size[0]/2,'y':re_size[1]/2}) 
     else:
 		result = {'angle':result['angle'],'length':0}
     cv2.imshow('catch',cam.mask)
     #cam.show_image()
     cv2.waitKey(1) 
-    print result['angle'],result['length']*3,(cam.centerx,cam.centery)
+    print result['angle'],result['length'],(cam.centerx,cam.centery)
     """
 
     测一下length的大小:(320,0) -> length = 200
-    				   (40,0) -> length = 30
+    				   (40,0) -> length = 30  speed*=3
+					   (60,0) -> length = 45  speed*=2 
+    距离小于多少时自动stop？
+    ->(120,90) 大约20左右合适
 	"""
-    speed = result['length']*3
+    speed = result['length']*2
+    speed = 
     if speed > max_speed:
         speed = max_speed
-    car.send_cmd(speed , result['angle'])
+   # car.send_cmd(speed , result['angle'])
 	
