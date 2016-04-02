6a4b3c1df946e7a17df89307b8164e42cde99238
root <root@smith-X4.(none)>
2016-04-02 13:32:44 +0800

    [BUGS]gix bugs in videorecord

--- cv/videorecord.py
+++ cv/videorecord.py
@@ -26,8 +26,6 @@ while(cap.isOpened()):
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    else:
-        break
 
 # Release everything if job is finished
 cap.release()
